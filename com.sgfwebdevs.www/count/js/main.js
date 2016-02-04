f97ac3410400a498e24948d4943206731600e6bd
Myke Bates <mbates@thealchemediaproject.com>
2016-02-03 18:20:31 -0600

    jksdfjklfsdjklfsdlkj

--- com.sgfwebdevs.www/count/js/main.js
+++ com.sgfwebdevs.www/count/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-    $("#countdown").countdown("2016/01/06 18:30:00", function(event) {
+    $("#countdown").countdown("2016/02/03 18:30:00", function(event) {
         $(this).text(event.strftime('%H:%M:%S'));
     }).on('finish.countdown', function() {
-        $('.container').append('<video id="quip" src="https://quipvid.s3.amazonaws.com/cc01920c07e817fdacc0950c8fe58cda.mp4" poster="https://s3.amazonaws.com/quipvid/ca6223ebe8b72bce3ea4c1fcc4bd5d0c_1.jpg" />');
+        $('.container').append('<video id="quip" src="https://quipvid.s3.amazonaws.com/1875380439cd03c4cd7b43ddb5c66cd0.mp4" poster="https://s3.amazonaws.com/quipvid/2ffcf67e623fefb1f946d88fd0f2b59b_1.jpg" />');
         $('#quip').animateCSS('slideInDown', function(){
             document.getElementById("quip").play();
         });
