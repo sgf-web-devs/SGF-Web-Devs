a86cccc6a2bc3e1c3159a7e1293755cc6dfcd943
Myke Bates <contact@mykebates.com>
2017-07-27 10:49:26 -0500

    Updating slack link. The heroku app took a dump a while back. I think because of the use of Slack's deprecated API system

--- com.sgfwebdevs.www/slack/index.php
+++ com.sgfwebdevs.www/slack/index.php
@@ -1,3 +1,4 @@
 <?php 
-    header( 'Location: https://sgf-web-devs-slack-invite.herokuapp.com/' ); 
+    //header( 'Location: https://sgf-web-devs-slack-invite.herokuapp.com/' );
+    header( 'Location: https://sgfwebdevs.typeform.com/to/A2HLlQ' );
 ?>
